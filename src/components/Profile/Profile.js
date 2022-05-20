05d1090c8e01a6e7ddd7bf3d5f692db83d6202ab
Lilia Lomaka <llomaka80@gmail.com>
2022-05-20 20:28:02 +0300

    Виведення профілю

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,31 +1,31 @@
-import ReactDOM from "react-dom";
-import user from "./user.json";
-
 export default function Profile(props) {
-  <div className="profile">
-    <div className="description">
-      <img
-        src={props.avatar}
-        alt={props.username + " avatar"}
-        className="avatar"
-      />
-      <p className="name">{props.username}</p>
-      <p className="tag">{props.tag}</p>
-      <p className="location">{props.location}</p>
+  const { avatar, username, tag, location, stats } = props;
+  return (
+    <div className="profile">
+      <div className="description">
+        <img
+          src={avatar}
+          alt={username + " avatar"}
+          className="avatar"
+        />
+        <p className="name">{username}</p>
+        <p className="tag">{"@" + tag}</p>
+        <p className="location">{location}</p>
+      </div>
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
     </div>
-    <ul className="stats">
-      <li>
-        <span className="label">Followers</span>
-        <span className="quantity">{props.stats.followers}</span>
-      </li>
-      <li>
-        <span className="label">Views</span>
-        <span className="quantity">{props.stats.views}</span>
-      </li>
-      <li>
-        <span className="label">Likes</span>
-        <span className="quantity">{props.stats.likes}</span>
-      </li>
-    </ul>
-  </div>
+  )
 }
