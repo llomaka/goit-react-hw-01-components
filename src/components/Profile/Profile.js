b8f0c4022f014f2cb2695549d911ab8be6534bac
Lilia Lomaka <llomaka80@gmail.com>
2022-05-20 19:49:05 +0300

    Створення розмітки профілю

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,31 @@
+import ReactDOM from "react-dom";
+import user from "./user.json";
+
+export default function Profile(props) {
+  <div className="profile">
+    <div className="description">
+      <img
+        src={props.avatar}
+        alt={props.username + " avatar"}
+        className="avatar"
+      />
+      <p className="name">{props.username}</p>
+      <p className="tag">{props.tag}</p>
+      <p className="location">{props.location}</p>
+    </div>
+    <ul className="stats">
+      <li>
+        <span className="label">Followers</span>
+        <span className="quantity">{props.stats.followers}</span>
+      </li>
+      <li>
+        <span className="label">Views</span>
+        <span className="quantity">{props.stats.views}</span>
+      </li>
+      <li>
+        <span className="label">Likes</span>
+        <span className="quantity">{props.stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+}
